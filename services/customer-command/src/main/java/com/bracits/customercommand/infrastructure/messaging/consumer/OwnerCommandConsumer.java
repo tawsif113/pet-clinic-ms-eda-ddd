391c8af72e4b3d8487f688d875a000063a163601
Mohammad Nayem <mohammad.nayem@bracits.com>
2025-09-01 12:10:14 +0600

    fix: merge issue

--- services/customer-command/src/main/java/com/bracits/customercommand/infrastructure/messaging/consumer/OwnerCommandConsumer.java
+++ services/customer-command/src/main/java/com/bracits/customercommand/infrastructure/messaging/consumer/OwnerCommandConsumer.java
@@ -1,16 +1,11 @@
 package com.bracits.customercommand.infrastructure.messaging.consumer;
 
-<<<<<<< Updated upstream
-=======
-import com.bracits.customercommand.application.command.CreatePetCommand;
-import com.bracits.sharedevent.event.infrastructure.messasing.constant.RabbitMQConstants;
->>>>>>> Stashed changes
 import com.bracits.customercommand.application.command.CreateOwnerCommand;
 import com.bracits.customercommand.application.command.CreatePetCommand;
 import com.bracits.customercommand.application.command.UpdateOwnerCommand;
 import com.bracits.customercommand.application.command.UpdatePetCommand;
 import com.bracits.customercommand.application.commandhandler.CommandHandlerRegistry;
-import com.bracits.sharedevent.messaging.RabbitMQConstants;
+import com.bracits.sharedevent.event.infrastructure.messasing.constant.RabbitMQConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
